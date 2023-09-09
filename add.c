71f03e04ee0970481766e609b5fc5cb97bf07c20
kiran kambale <kambalekiran10@gmail.com>
2023-09-09 12:43:39 +0530

    sum

---
+++ add.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+    int x, y, sum;
+    printf("enter the first number: ");
+    scanf("%d", &x);
+    printf("enter the seconde number:");
+    scanf("%d",&y);
+    sum= x+y;
+    printf("your sum is: %d", sum);
+    return 0;
+    
+}
